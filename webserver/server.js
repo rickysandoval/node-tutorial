f40681095ddad7266e8c041c291bcf4147944c81
Ricky Sandoval <rsandoval@arrow.com>
2019-01-09 08:52:14 -0700

    add heroku stuff

--- webserver/server.js
+++ webserver/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs'); // handlebars
 const fs = require('fs');
+const port = process.env.port || 3000;
 
 let app = express();
 
@@ -52,6 +53,6 @@ app.get('/bad', (req, res) => {
 });
 
 // Bind application to port on machine
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
